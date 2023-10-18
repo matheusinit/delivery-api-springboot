c7bd6e5f71b276e3464b78bb37c152b99daa72cc
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-18 15:06:59 -0300

    feat: given product id when get stock by product then return product stock

--- src/main/java/com/deliveryapirest/controller/GetStockByProductController.java
+++ src/main/java/com/deliveryapirest/controller/GetStockByProductController.java
@@ -10,6 +10,17 @@ public class GetStockByProductController {
 
   @GetMapping("/stock/{productId}")
   ResponseEntity<?> getStockByProduct(@PathVariable String productId) {
-    return ResponseEntity.ok().body(null);
+    class ProductStock {
+      public final String id = "1";
+      public final String productId = "1";
+      public final int quantity = 1;
+      public final String createdAt = "2021-01-01T00:00:00";
+      public final String updatedAt = null;
+      public final String deletedAt = null;
+    }
+
+    var productStock = new ProductStock();
+
+    return ResponseEntity.ok().body(productStock);
   }
 }
