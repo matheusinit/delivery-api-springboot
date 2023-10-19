88c6cbeaf6dc22d4aa1505d3fb58ca0c718fbf53
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-19 13:51:02 -0300

    refactor: use variable to give meaning to random uuid generated

--- src/test/java/com/deliveryapirest/StockUnitTest.java
+++ src/test/java/com/deliveryapirest/StockUnitTest.java
@@ -10,8 +10,9 @@ class StockUnitTest {
 
   @Test
   void ensureWhenStockIsCreatedThenShouldHaveId() {
+    var productId = UUID.randomUUID();
 
-    var stock = new Stock(UUID.randomUUID(), 1);
+    var stock = new Stock(productId, 1);
 
     assertNotNull(stock.getId());
   }
