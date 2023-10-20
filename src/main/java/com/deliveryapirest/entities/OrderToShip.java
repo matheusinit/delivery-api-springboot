621b42e2f1a90a71246ef3f59ad9e8b9ceb400d9
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-20 15:00:39 -0300

    feat: ensure when product is is provided then should have product id

--- src/main/java/com/deliveryapirest/entities/OrderToShip.java
+++ src/main/java/com/deliveryapirest/entities/OrderToShip.java
@@ -4,12 +4,21 @@ import java.util.UUID;
 
 public class OrderToShip {
   private UUID id;
+  private UUID productId;
 
   public OrderToShip() {
     this.id = UUID.randomUUID();
   }
 
+  public OrderToShip(UUID productId) {
+    this.productId = productId;
+  }
+
   public UUID getId() {
     return id;
+  }
+
+  public UUID getProductId() {
+    return productId;
   }
 }
