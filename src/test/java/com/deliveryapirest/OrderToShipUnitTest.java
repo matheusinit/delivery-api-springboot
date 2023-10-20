9943a64f658bcb28de0692f465f7844f25cfd416
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-20 15:25:59 -0300

    test: ensure when valid data is provided then should create random uuid

--- src/test/java/com/deliveryapirest/OrderToShipUnitTest.java
+++ src/test/java/com/deliveryapirest/OrderToShipUnitTest.java
@@ -75,4 +75,14 @@ class OrderToShipUnitTest {
 
     assertThat(orderToShip.getDeletedAt(), is(nullValue()));
   }
+
+  @Test
+  void ensureWhenValidDataIsProvidedThenShouldCreateRandomUUID() {
+    var productId = UUID.randomUUID();
+    var quantity = 2;
+
+    var orderToShip = new OrderToShip(productId, quantity);
+
+    assertThat(orderToShip.getId(), is(instanceOf(UUID.class)));
+  }
 }
