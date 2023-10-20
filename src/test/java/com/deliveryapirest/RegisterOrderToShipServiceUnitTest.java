b9276e22601c82ebf8014ec020a06963b37ead5b
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-20 17:27:51 -0300

    test: ensure when deleted at provided is not null then should not register

--- src/test/java/com/deliveryapirest/RegisterOrderToShipServiceUnitTest.java
+++ src/test/java/com/deliveryapirest/RegisterOrderToShipServiceUnitTest.java
@@ -3,38 +3,47 @@
 // 3. Consumer will pass order object to a Service class to register order to
 // ship
 
-// BUSINESS LOGIC OF REGISTER ORDER TO SHIP
-// If deleted at of order is not null should not register
-// if status order is not SENT should not register
-
 package com.deliveryapirest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import com.deliveryapirest.data.Order;
+import com.deliveryapirest.data.OrderStatus;
 import com.deliveryapirest.entities.OrderToShip;
 import com.deliveryapirest.repositories.inMemory.InMemoryOrderToShipRepository;
 import com.deliveryapirest.services.RegisterOrderToShipService;
+import java.time.ZonedDateTime;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-
-enum Status {
-  NOT_SENT,
-  CANCELLED
-}
 
 class RegisterOrderToShipServiceUnitTest {
   @Test
   void ensureWhenStatusProvidedIsNotNotSentThenShouldNotRegister() {
-    var order = new Order();
+    var order = new Order(OrderStatus.CANCELLED);
     var repository = new InMemoryOrderToShipRepository();
     var sut = new RegisterOrderToShipService(repository);
 
     sut.register(order);
 
     var list = repository.findAll();
-    var expected = not(hasItem(Matchers.<OrderToShip>hasProperty("status", is(Status.CANCELLED))));
+    var expected =
+        not(hasItem(Matchers.<OrderToShip>hasProperty("status", is(OrderStatus.CANCELLED))));
+    assertThat(list.size(), is(0));
+    assertThat(list, expected);
+  }
+
+  @Test
+  void ensureWhenDeletedAtProvidedIsNotNullThenShouldNotRegister() {
+    var deletedAt = ZonedDateTime.now();
+    var order = new Order(deletedAt);
+    var repository = new InMemoryOrderToShipRepository();
+    var sut = new RegisterOrderToShipService(repository);
+
+    sut.register(order);
+
+    var list = repository.findAll();
+    var expected = not(hasItem(Matchers.<OrderToShip>hasProperty("deletedAt", is(deletedAt))));
     assertThat(list.size(), is(0));
     assertThat(list, expected);
   }
