b0cece5e71b278fe576627ba4111aab185b938a8
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-25 17:01:38 -0300

    test: ensure when given order with status different than not sent when register then should not register

---
+++ src/integrationTest/java/com/deliveryapirest/RegisterOrderToShipServiceIntegrationTest.java
@@ -0,0 +1,35 @@
+package com.deliveryapirest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import com.deliveryapirest.data.Order;
+import com.deliveryapirest.data.OrderStatus;
+import com.deliveryapirest.entities.OrderToShip;
+import com.deliveryapirest.repositories.protocols.OrderToShipRepository;
+import com.deliveryapirest.services.RegisterOrderToShipService;
+import java.util.Collection;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RegisterOrderToShipServiceIntegrationTest {
+
+  @Autowired OrderToShipRepository repository;
+
+  @Test
+  void givenOrderWithStatusDifferentThanNotSent_whenRegister_thenShouldNotRegister() {
+    var sut = new RegisterOrderToShipService(repository);
+    var order = new Order(OrderStatus.CANCELLED, 1);
+
+    sut.register(order);
+
+    var list = repository.findAll();
+    var expected =
+        not(hasItem(Matchers.<OrderToShip>hasProperty("status", is(OrderStatus.CANCELLED))));
+    assertThat(((Collection<?>) list).size(), is(0));
+    assertThat(list, expected);
+  }
+}
--- src/main/java/com/deliveryapirest/repositories/inMemory/InMemoryOrderToShipRepository.java
+++ src/main/java/com/deliveryapirest/repositories/inMemory/InMemoryOrderToShipRepository.java
@@ -2,16 +2,16 @@ package com.deliveryapirest.repositories.inMemory;
 
 import com.deliveryapirest.entities.OrderToShip;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 public class InMemoryOrderToShipRepository {
-  private Collection<OrderToShip> orders;
+  private List<OrderToShip> orders;
 
   public InMemoryOrderToShipRepository() {
     this.orders = new ArrayList<OrderToShip>();
   }
 
-  public Collection<OrderToShip> findAll() {
+  public List<OrderToShip> findAll() {
     return orders;
   }
 
