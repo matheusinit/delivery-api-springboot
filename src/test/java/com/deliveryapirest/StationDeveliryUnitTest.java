f05cc5a73630cac67dcb05aa9781d759a7cc9133
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-27 13:29:50 -0300

    test: given none fields when station delivery is created then should throw error

--- src/test/java/com/deliveryapirest/StationDeveliryUnitTest.java
+++ src/test/java/com/deliveryapirest/StationDeveliryUnitTest.java
@@ -9,6 +9,14 @@ import org.junit.jupiter.api.Test;
 class StationDeliveryUnitTest {
 
   @Test
+  void givenNoneFields_whenStationDeliveryIsCreated_thenShouldThrowError() {
+    Exception error =
+        assertThrows(Exception.class, () -> new StationDelivery(null, null, null, null));
+
+    assertEquals("Name, Zip code, Latitude and Longitude is required", error.getMessage());
+  }
+
+  @Test
   void givenName_whenAnyOtherFieldIsProvided_thenShouldThrowError() {
     var name = "Rio Grande do Norte\'s Station Delivery";
 
