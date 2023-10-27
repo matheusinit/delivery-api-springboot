53b8393d17464351672f09c91c16cb3c21e5ef3b
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-27 13:29:42 -0300

    feat: given none fields when station delivery is created then should throw error

--- src/main/java/com/deliveryapirest/entities/StationDelivery.java
+++ src/main/java/com/deliveryapirest/entities/StationDelivery.java
@@ -4,6 +4,10 @@ public class StationDelivery {
 
   public StationDelivery(String name, String zipCode, Double latitude, Double longitude)
       throws Exception {
+    if (name == null && zipCode == null && latitude == null && longitude == null) {
+      throw new Exception("Name, Zip code, Latitude and Longitude is required");
+    }
+
     throw new Exception("Zip code, latitude and longitude is required");
   }
 }
