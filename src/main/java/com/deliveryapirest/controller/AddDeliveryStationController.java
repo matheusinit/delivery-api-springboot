75ebac1cc5836b5c5c2aa155c293a41a0a09460f
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-28 08:03:08 -0300

    feat: given only name and zip code when any other field is not provided then should get bad request

--- src/main/java/com/deliveryapirest/controller/AddDeliveryStationController.java
+++ src/main/java/com/deliveryapirest/controller/AddDeliveryStationController.java
@@ -4,13 +4,39 @@ import com.deliveryapirest.errors.BadRequestError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+class DeliveryStationLocation {
+  private String name;
+  private String zipCode;
+
+  public DeliveryStationLocation(String name, String zipCode) {
+    this.name = name;
+    this.zipCode = zipCode;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getZipCode() {
+    return this.zipCode;
+  }
+}
 
 @RestController()
 public class AddDeliveryStationController {
   @PostMapping("/station")
-  ResponseEntity<?> addDeliveryStation() {
+  ResponseEntity<?> addDeliveryStation(
+      @RequestBody DeliveryStationLocation deliveryStationLocation) {
+
+    if (deliveryStationLocation.getName() != null && deliveryStationLocation.getZipCode() == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body(BadRequestError.make("Zip code, Latitude and Longitude is required"));
+    }
+
     return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-        .body(BadRequestError.make("Zip code, Latitude and Longitude is required"));
+        .body(BadRequestError.make("Latitude and Longitude is required"));
   }
 }
