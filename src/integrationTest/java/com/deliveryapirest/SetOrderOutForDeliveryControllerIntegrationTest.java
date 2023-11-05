592cf1cc4b9233f47cb626d6c2929212a4a327cc
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-05 18:26:47 -0300

    test: ensure given order id when order is not found then should get not found

--- src/integrationTest/java/com/deliveryapirest/SetOrderOutForDeliveryControllerIntegrationTest.java
+++ src/integrationTest/java/com/deliveryapirest/SetOrderOutForDeliveryControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import io.restassured.RestAssured;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,9 +31,29 @@ class SetOrderOutForDeliveryControllerIntegrationTest {
             .extract()
             .response();
 
+    response.getBody().prettyPrint();
     var responseBody = response.getBody().jsonPath();
     var badRequestCode = 400;
     assertThat(response.getStatusCode(), is(badRequestCode));
-    assertThat(responseBody.get("message"), is("Order id is required"));
+    assertThat(responseBody.get("error"), is("Bad Request"));
+  }
+
+  @Test
+  void ensureGivenOrderId_whenOrderIsNotFound_thenShouldGetNotFound() {
+    var randomUuid = UUID.randomUUID();
+
+    var response =
+        RestAssured.given()
+            .accept("application/json")
+            .contentType("application/json")
+            .when()
+            .post("/order/" + randomUuid + "/delivery")
+            .then()
+            .extract()
+            .response();
+
+    var responseBody = response.getBody().jsonPath();
+    assertThat(response.getStatusCode(), is(404));
+    assertThat(responseBody.get("message"), is("Order not found with given id"));
   }
 }
