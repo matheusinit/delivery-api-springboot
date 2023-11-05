76e5bea1540cfa962d31c5a9b7e5a498d295b9f0
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-05 18:26:37 -0300

    feat: ensure given order id when order is not found then should get not found

--- src/main/java/com/deliveryapirest/controller/SetOrderOutForDeliveryController.java
+++ src/main/java/com/deliveryapirest/controller/SetOrderOutForDeliveryController.java
@@ -1,6 +1,9 @@
 package com.deliveryapirest.controller;
 
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class SetOrderOutForDeliveryController {
 
   @PostMapping("/order/{id}/delivery")
-  public ResponseEntity<?> setOrderOutForDelivery() {
-    return ResponseEntity.badRequest().body(new Error("Order id is required"));
+  public ResponseEntity<?> setOrderOutForDelivery(@PathVariable UUID id) {
+    if (id == null) {
+      return ResponseEntity.badRequest().body(new Error("Order id is required"));
+    }
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(new Error("Order not found with given id"));
   }
 }
