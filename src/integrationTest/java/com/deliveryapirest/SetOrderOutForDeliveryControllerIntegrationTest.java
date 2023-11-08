f1cce7125ea69b0b213d28d3ab6be4c5ffbac816
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-07 23:58:21 -0300

    test: ensure given order id when order has status in delivery then should get bad request

--- src/integrationTest/java/com/deliveryapirest/SetOrderOutForDeliveryControllerIntegrationTest.java
+++ src/integrationTest/java/com/deliveryapirest/SetOrderOutForDeliveryControllerIntegrationTest.java
@@ -109,4 +109,26 @@ class SetOrderOutForDeliveryControllerIntegrationTest {
     assertThat(response.getStatusCode(), is(400));
     assertThat(responseBody.get("message"), is(expectedMessageError));
   }
+
+  @Test
+  void ensureGivenOrderId_whenOrderHasStatusInDelivery_thenShouldGetBadRequest() {
+    var productId = UUID.randomUUID();
+    var order = new OrderToShip(productId, OrderStatus.IN_DELIVERY, 1);
+    repository.save(order);
+
+    var response =
+        RestAssured.given()
+            .accept("application/json")
+            .contentType("application/json")
+            .when()
+            .post("/order/" + order.getId() + "/delivery")
+            .then()
+            .extract()
+            .response();
+
+    var responseBody = response.getBody().jsonPath();
+    var expectedMessageError = "Order is in delivery. Cannot set it out for delivery!";
+    assertThat(response.getStatusCode(), is(400));
+    assertThat(responseBody.get("message"), is(expectedMessageError));
+  }
 }
