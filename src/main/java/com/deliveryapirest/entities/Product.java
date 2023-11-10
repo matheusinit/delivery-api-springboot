5fc0b0de163725d0b192e86ee192134674ce07b5
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-10 15:06:15 -0300

    feat: ensure given price is not provided when is requested to register product then is returned an error

--- src/main/java/com/deliveryapirest/entities/Product.java
+++ src/main/java/com/deliveryapirest/entities/Product.java
@@ -5,4 +5,8 @@ public class Product {
   public Product() throws Exception {
     throw new Exception("Product name is required");
   }
+
+  public Product(String name) throws Exception {
+    throw new Exception("Price is required");
+  }
 }
