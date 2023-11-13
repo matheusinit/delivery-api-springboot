1dc0b1d355571742e5b489728a591ed031d7288f
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-13 18:11:28 -0300

    test: ensure given name is not provided when register product then get bad request

---
+++ src/integrationTest/java/com/deliveryapirest/RegisterProductControllerIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.deliveryapirest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RegisterProductControllerIntegrationTest {
+  @LocalServerPort private int port;
+
+  @BeforeAll
+  void setup() {
+    RestAssured.baseURI = "http://localhost:" + port;
+  }
+
+  @Test
+  void givenNameIsNotProvided_whenRegisterProduct_thenGetBadRequest() {
+    var response =
+        RestAssured.given()
+            .accept("application/json")
+            .contentType("application/json")
+            .when()
+            .post("/product")
+            .then()
+            .extract()
+            .response();
+
+    assertThat(response.statusCode(), equalTo(400));
+    assertThat(
+        response.body().jsonPath().getString("message"), equalTo("Product name is required"));
+  }
+}
