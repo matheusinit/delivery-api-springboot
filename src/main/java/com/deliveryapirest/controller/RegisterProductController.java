5ba3e43b1842721520fbc1228308fb15a477e56b
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-14 09:04:58 -0300

    feat: given only name when register product then get created

--- src/main/java/com/deliveryapirest/controller/RegisterProductController.java
+++ src/main/java/com/deliveryapirest/controller/RegisterProductController.java
@@ -1,6 +1,7 @@
 package com.deliveryapirest.controller;
 
 import com.deliveryapirest.data.RegisterProductInput;
+import com.deliveryapirest.entities.Product;
 import com.deliveryapirest.errors.BadRequestError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,19 +14,28 @@ public class RegisterProductController {
 
   @PostMapping("/product")
   ResponseEntity<?> registerProduct(@RequestBody RegisterProductInput input) {
+    try {
+      var product = new Product(input.getName(), input.getDescription());
 
-    if (input.getName() == null) {
+      return ResponseEntity.status(HttpStatus.CREATED).body(product);
+
+    } catch (Exception exception) {
+      if (input.getName() == null) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(BadRequestError.make("Product name is required"));
+      }
+
+      if (input.getDescription() != null
+          && !input.getDescription().isEmpty()
+          && input.getDescription().length() < 10) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(BadRequestError.make("Description cannot have less than 10 characters"));
+      }
+
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-          .body(BadRequestError.make("Product name is required"));
+          .body(
+              BadRequestError.make(
+                  "Description cannot be empty, must have at least 10 characters"));
     }
-
-    if (input.getDescription() != "" && input.getDescription().length() < 10) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-          .body(BadRequestError.make("Description cannot have less than 10 characters"));
-    }
-
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-        .body(
-            BadRequestError.make("Description cannot be empty, must have at least 10 characters"));
   }
 }
