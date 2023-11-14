06f5b653fda754eeb83ef68629db1f3052d07e48
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-14 08:31:19 -0300

    test: ensure given description as empty when register product then get bad request

--- src/integrationTest/java/com/deliveryapirest/RegisterProductControllerIntegrationTest.java
+++ src/integrationTest/java/com/deliveryapirest/RegisterProductControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package com.deliveryapirest;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+import com.deliveryapirest.data.RegisterProductInput;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -20,10 +21,13 @@ class RegisterProductControllerIntegrationTest {
 
   @Test
   void givenNameIsNotProvided_whenRegisterProduct_thenGetBadRequest() {
+    var input = new RegisterProductInput(null);
+
     var response =
         RestAssured.given()
             .accept("application/json")
             .contentType("application/json")
+            .body(input)
             .when()
             .post("/product")
             .then()
@@ -33,5 +37,27 @@ class RegisterProductControllerIntegrationTest {
     var responseBody = response.body().jsonPath();
     assertThat(response.statusCode(), equalTo(400));
     assertThat(responseBody.get("message"), equalTo("Product name is required"));
+  }
+
+  @Test
+  void givenDescriptionAsEmpty_whenRegisterProduct_thenGetBadRequest() {
+    var input = new RegisterProductInput("name", "");
+
+    var response =
+        RestAssured.given()
+            .accept("application/json")
+            .contentType("application/json")
+            .body(input)
+            .when()
+            .post("/product")
+            .then()
+            .extract()
+            .response();
+
+    var responseBody = response.body().jsonPath();
+    assertThat(response.statusCode(), equalTo(400));
+    assertThat(
+        responseBody.get("message"),
+        is("Description cannot be empty, must have at least 10 characters"));
   }
 }
