5df543cd228037cac2c392972aa0a2e34d766074
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-14 22:14:27 -0300

    test: given products stored in database when list products then product list are returned with same size

---
+++ src/integrationTest/java/com/deliveryapirest/ListProductsControllerIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.deliveryapirest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import com.deliveryapirest.entities.Product;
+import com.deliveryapirest.repositories.protocols.ProductRepository;
+import io.restassured.RestAssured;
+import net.datafaker.Faker;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ListProductsControllerIntegrationTest {
+  @LocalServerPort private Integer port;
+
+  @Autowired ProductRepository repository;
+
+  @BeforeAll
+  void setup() {
+    RestAssured.baseURI = "http://localhost:" + port;
+  }
+
+  @Test
+  void givenProductsStoredInDatabase_whenListProducts_thenProductListAreReturnedWithSameSize()
+      throws Exception {
+    var faker = new Faker();
+    var name = faker.commerce().productName();
+    var description = faker.lorem().sentence(10);
+    this.repository.save(new Product(name, description));
+    var secondName = faker.commerce().productName();
+    var secondDescription = faker.lorem().sentence(10);
+    this.repository.save(new Product(secondName, secondDescription));
+
+    var response =
+        RestAssured.given()
+            .accept("application/json")
+            .when()
+            .get("/product")
+            .then()
+            .extract()
+            .response();
+
+    var responseBody = response.getBody().jsonPath();
+    responseBody.prettyPrint();
+    assertThat(responseBody.getList("$"), hasSize(2));
+  }
+}
