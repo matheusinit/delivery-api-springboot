76adc6f51e43c043c53ed39b5482746abb2f6615
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-15 13:12:26 -0300

    feat: given error happened when list products then return internal server error

--- src/main/java/com/deliveryapirest/controller/ListProductsController.java
+++ src/main/java/com/deliveryapirest/controller/ListProductsController.java
@@ -1,6 +1,8 @@
 package com.deliveryapirest.controller;
 
+import com.deliveryapirest.errors.InternalServerError;
 import com.deliveryapirest.repositories.protocols.ProductRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +16,16 @@ public class ListProductsController {
   }
 
   @GetMapping("/product")
-  ResponseEntity<?> listProducts() {
-    var products = this.repository.findAll();
+  public ResponseEntity<?> listProducts() {
+    try {
+      var products = this.repository.findAll();
 
-    return ResponseEntity.ok(products);
+      return ResponseEntity.ok(products);
+
+    } catch (Exception exception) {
+      var errorMessage =
+          InternalServerError.make("An internal server error occured. Please try again later.");
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+    }
   }
 }
