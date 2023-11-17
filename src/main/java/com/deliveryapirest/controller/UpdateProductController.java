e8b34a8d104aab97d61ba3101179fed26c87d1b3
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-17 09:42:49 -0300

    feat: given name as empty when update product then return bad request

--- src/main/java/com/deliveryapirest/controller/UpdateProductController.java
+++ src/main/java/com/deliveryapirest/controller/UpdateProductController.java
@@ -6,13 +6,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+class UpdateProductInput {
+  public String name;
+  public String description;
+}
 
 @RestController
 public class UpdateProductController {
 
   @PatchMapping("/product/{id}")
-  ResponseEntity<?> updateProduct(@PathVariable UUID id) {
+  ResponseEntity<?> updateProduct(@PathVariable UUID id, @RequestBody UpdateProductInput input) {
+
+    if (input.name == "") {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body(
+              BadRequestError.make(
+                  "Name cannot be null and description must be not null, at least one of them must"
+                      + " be provided"));
+    }
 
     return ResponseEntity.status(HttpStatus.BAD_REQUEST)
         .body(
