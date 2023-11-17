5e1a0b49ddabdf726d4bbc9bee429fe88af07938
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-17 16:32:10 -0300

    test: given a product already deleted when delete product then return not found

--- src/integrationTest/java/com/deliveryapirest/DeleteProductControllerIntegrationTest.java
+++ src/integrationTest/java/com/deliveryapirest/DeleteProductControllerIntegrationTest.java
@@ -3,16 +3,22 @@ package com.deliveryapirest;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+import com.deliveryapirest.entities.Product;
+import com.deliveryapirest.repositories.protocols.ProductRepository;
 import io.restassured.RestAssured;
 import java.util.UUID;
+import net.datafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DeleteProductControllerIntegrationTest {
   @LocalServerPort private Integer port;
+
+  @Autowired ProductRepository repository;
 
   @BeforeAll
   void setup() {
@@ -34,6 +40,30 @@ class DeleteProductControllerIntegrationTest {
             .response();
 
     response.getBody().prettyPrint();
+
+    assertThat(response.getStatusCode(), is(404));
+    assertThat(response.getBody().jsonPath().get("message"), is("Product not found"));
+  }
+
+  @Test
+  void givenAProductAlreadyDeleted_whenDeleteProduct_thenReturnNotFound() throws Exception {
+    var faker = new Faker();
+    var name = faker.commerce().productName();
+    var description = faker.lorem().maxLengthSentence(10);
+    var product = new Product(name, description);
+    repository.save(product);
+    var id = product.getId();
+    repository.softDelete(id);
+
+    var response =
+        RestAssured.given()
+            .accept("application/json")
+            .contentType("application/json")
+            .when()
+            .delete("/product/" + id)
+            .then()
+            .extract()
+            .response();
 
     assertThat(response.getStatusCode(), is(404));
     assertThat(response.getBody().jsonPath().get("message"), is("Product not found"));
