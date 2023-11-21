ef5697e58a23bbc2c55348f9ede2eef650dead34
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-21 09:59:08 -0300

    feat: given quantity is not provided when set stock by product then return bad request

--- src/main/java/com/deliveryapirest/controller/SetStockByProductController.java
+++ src/main/java/com/deliveryapirest/controller/SetStockByProductController.java
@@ -1,15 +1,31 @@
 package com.deliveryapirest.controller;
 
 import com.deliveryapirest.errors.BadRequestError;
+import com.deliveryapirest.repositories.protocols.ProductRepository;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class SetStockByProductController {
+  ProductRepository productRepository;
+
+  public SetStockByProductController(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
 
   @PostMapping("/product/{id}/stock")
-  ResponseEntity<?> setStockByProduct() {
-    return ResponseEntity.status(404).body(BadRequestError.make("Product not found"));
+  ResponseEntity<?> setStockByProduct(@PathVariable UUID id) {
+    var productValue = productRepository.findById(id);
+
+    if (productValue.isEmpty()) {
+      return ResponseEntity.status(404).body(BadRequestError.make("Product not found"));
+    }
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(BadRequestError.make("Quantity must be provided"));
   }
 }
