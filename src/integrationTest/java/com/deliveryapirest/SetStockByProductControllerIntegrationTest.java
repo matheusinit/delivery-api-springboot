b1a228f984d15a5b02789b802b1af18ed0520adc
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-21 09:59:17 -0300

    test: given quantity is not provided when set stock by product then return bad request

--- src/integrationTest/java/com/deliveryapirest/SetStockByProductControllerIntegrationTest.java
+++ src/integrationTest/java/com/deliveryapirest/SetStockByProductControllerIntegrationTest.java
@@ -3,16 +3,22 @@ package com.deliveryapirest;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import com.deliveryapirest.entities.Product;
+import com.deliveryapirest.repositories.protocols.ProductRepository;
 import io.restassured.RestAssured;
 import java.util.UUID;
+import net.datafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SetStockByProductControllerIntegrationTest {
   @LocalServerPort Integer port;
+
+  @Autowired ProductRepository productRepository;
 
   @BeforeAll
   void setupRestAssured() {
@@ -35,5 +41,28 @@ class SetStockByProductControllerIntegrationTest {
     var responseBody = response.getBody().jsonPath();
     assertThat(response.statusCode(), is(404));
     assertThat(responseBody.get("message"), is("Product not found"));
+  }
+
+  @Test
+  void givenQuantityIsNotProvided_whenSetStockByProduct_thenReturnBadRequest() throws Exception {
+    var faker = new Faker();
+    var name = faker.commerce().productName();
+    var description = faker.lorem().maxLengthSentence(10);
+    var product = new Product(name, description);
+    productRepository.save(product);
+    var id = product.getId();
+
+    var response =
+        RestAssured.given()
+            .accept("application/json")
+            .accept("application/json")
+            .post("/product/" + id + "/stock")
+            .then()
+            .extract()
+            .response();
+
+    var responseBody = response.getBody().jsonPath();
+    assertThat(response.statusCode(), is(400));
+    assertThat(responseBody.get("message"), is("Quantity must be provided"));
   }
 }
