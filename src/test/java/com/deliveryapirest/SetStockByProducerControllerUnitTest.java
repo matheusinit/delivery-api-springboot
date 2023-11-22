8327e825c6e16f916959b7d0fa93464e2c5531fe
matheusinit <matheus.oliveira.s@protonmail.com>
2023-11-22 11:54:23 -0300

    test: given a error thrown when set stock by productt then return internal server error

---
+++ src/test/java/com/deliveryapirest/SetStockByProducerControllerUnitTest.java
@@ -0,0 +1,32 @@
+package com.deliveryapirest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.deliveryapirest.controller.SetStockByProductController;
+import com.deliveryapirest.data.SetStockByProductInput;
+import com.deliveryapirest.errors.InvalidOperationError;
+import com.deliveryapirest.services.SetStockByProductService;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+class SetStockByProducerControllerUnitTest {
+  @Test
+  void givenAErrorThrown_whenSetStockByProduct_thenReturnInternalServerError()
+      throws InvalidOperationError {
+    var service = mock(SetStockByProductService.class);
+    var sut = new SetStockByProductController(service);
+    when(service.setStockByProduct(any(), any())).thenThrow(new RuntimeException());
+    var productId = UUID.randomUUID();
+    var input = new SetStockByProductInput();
+    input.quantity = 1;
+
+    var response = sut.setStockByProduct(productId, input);
+
+    assertThat(response.getStatusCode(), is(HttpStatus.INTERNAL_SERVER_ERROR));
+  }
+}
