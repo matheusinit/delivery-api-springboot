44ab06114b0850ed61ebeb35bca64180ec3f2828
suetham <matheus.oliveira.s@protonmail.com>
2024-02-19 10:07:09 -0300

    test: ensure when there is any delivery station then should return list with items

--- src/test/java/com/deliveryapirest/GetDeliveryStationsService.java
+++ src/test/java/com/deliveryapirest/GetDeliveryStationsService.java
@@ -2,6 +2,9 @@ package com.deliveryapirest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.deliveryapirest.entities.DeliveryStation;
+import com.deliveryapirest.repositories.inMemory.InMemoryDeliveryStationRepository;
+import com.deliveryapirest.repositories.protocols.DeliveryStationRepository;
 import com.deliveryapirest.services.GetDeliveryStationsService;
 
 class GetDeliveryStationsServiceUnitTest {
@@ -12,5 +15,19 @@ class GetDeliveryStationsServiceUnitTest {
     var deliveryStations = sut.getDeliveryStations();
 
     assertEquals(0, deliveryStations.size());
+  }
+
+  void ensureWhenThereIsAnyDeliveryStationThenShouldReturnListWithItems()
+      throws Exception {
+    var repository = new InMemoryDeliveryStationRepository();
+    var deliveryStation =
+        new DeliveryStation("Rio Grande do Norte\'s Station Delivery",
+                            "59064-625", -5.826694, -35.2144);
+    repository.save(deliveryStation);
+    var sut = new GetDeliveryStationsService(repository);
+
+    var deliveryStations = sut.getDeliveryStations();
+
+    assertEquals(1, deliveryStations.size());
   }
 }
