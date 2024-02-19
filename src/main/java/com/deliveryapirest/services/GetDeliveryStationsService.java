e6a3bfc181af92167c0cf34bc1b90fda1efff085
suetham <matheus.oliveira.s@protonmail.com>
2024-02-19 09:56:46 -0300

    feat: ensure when there isn't any delivery stations then should return empty list

---
+++ src/main/java/com/deliveryapirest/services/GetDeliveryStationsService.java
@@ -0,0 +1,11 @@
+package com.deliveryapirest.services;
+
+import com.deliveryapirest.entities.DeliveryStation;
+import java.util.List;
+import java.util.Vector;
+
+public class GetDeliveryStationsService {
+  public List<DeliveryStation> getDeliveryStations() {
+    return new Vector<DeliveryStation>();
+  }
+}
