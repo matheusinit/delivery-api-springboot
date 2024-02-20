ebbac611ed71951711b2ee4e8880fc3880f997ac
suetham <matheus.oliveira.s@protonmail.com>
2024-02-20 09:15:49 -0300

    test: ensure when there is a delivery station then should return the delivery station stored

--- src/test/java/com/deliveryapirest/GetDeliveryStationsServiceUnitTest.java
+++ src/test/java/com/deliveryapirest/GetDeliveryStationsServiceUnitTest.java
@@ -34,4 +34,20 @@ class GetDeliveryStationsServiceUnitTest {
 
     assertThat(deliveryStations.size(), greaterThan(0));
   }
+
+  @Test
+  void
+  ensureWhenThereIsADeliveryStationThenShouldReturnTheDeliveryStationStored()
+      throws Exception {
+    var repository = new InMemoryDeliveryStationRepository();
+    var deliveryStation =
+        new DeliveryStation("Rio Grande do Norte\'s Station Delivery",
+                            "59064-625", -5.826694, -35.2144);
+    repository.save(deliveryStation);
+    var sut = new GetDeliveryStationsService(repository);
+
+    var deliveryStations = sut.getDeliveryStations();
+
+    assertThat(deliveryStations.get(0), equalTo(deliveryStation));
+  }
 }
