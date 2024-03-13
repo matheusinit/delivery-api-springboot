b46c694889506248a32a3c128ccf7b8da4f5c07e
suetham <matheus.oliveira.s@protonmail.com>
2024-03-12 23:43:42 -0300

    test: ensure status input is provided

--- src/test/java/com/deliveryapirest/order/SetOrderOutForDeliveryControllerUnitTest.java
+++ src/test/java/com/deliveryapirest/order/SetOrderOutForDeliveryControllerUnitTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 import com.deliveryapirest.controller.order.SetOrderOutForDeliveryController;
+import com.deliveryapirest.data.StatusInput;
 import com.deliveryapirest.repositories.inMemory.InMemoryOrderToShipRepository;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
@@ -18,8 +19,9 @@ class SetOrderOutForDeliveryControllerUnitTest {
     var mock = mock(InMemoryOrderToShipRepository.class);
     when(mock.findById(orderId)).thenThrow(new RuntimeException());
     var sut = new SetOrderOutForDeliveryController(mock);
+    var statusInput = new StatusInput();
 
-    var response = sut.setOrderOutForDelivery(orderId);
+    var response = sut.setOrderOutForDelivery(orderId, statusInput);
 
     assertEquals(HttpStatus.valueOf(500), response.getStatusCode());
   }
