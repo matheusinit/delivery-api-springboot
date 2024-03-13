de307c60515340ef0ff8bf27ea6417294b64663a
suetham <matheus.oliveira.s@protonmail.com>
2024-03-12 23:43:07 -0300

    test: ensure given order id when order status is not given then should get bad request

--- src/integrationTest/java/com/deliveryapirest/order/SetOrderOutForDeliveryControllerIntegrationTest.java
+++ src/integrationTest/java/com/deliveryapirest/order/SetOrderOutForDeliveryControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import com.deliveryapirest.data.OrderStatus;
+import com.deliveryapirest.data.StatusInput;
 import com.deliveryapirest.entities.OrderToShip;
 import com.deliveryapirest.repositories.protocols.OrderToShipRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -30,9 +31,12 @@ class SetOrderOutForDeliveryControllerIntegrationTest {
 
   @Test
   void ensureWhenOrderIdIsNotProvided_thenShouldGetBadRequest() {
+    var status = new StatusInput(null);
+
     var response = RestAssured.given()
                        .accept("application/json")
                        .contentType("application/json")
+                       .body(status)
                        .when()
                        .post("/order/null/delivery")
                        .then()
@@ -48,10 +52,12 @@ class SetOrderOutForDeliveryControllerIntegrationTest {
   @Test
   void ensureGivenOrderId_whenOrderIsNotFound_thenShouldGetNotFound() {
     var randomUuid = UUID.randomUUID();
+    var status = new StatusInput(null);
 
     var response = RestAssured.given()
                        .accept("application/json")
                        .contentType("application/json")
+                       .body(status)
                        .when()
                        .post("/order/" + randomUuid + "/delivery")
                        .then()
@@ -70,10 +76,12 @@ class SetOrderOutForDeliveryControllerIntegrationTest {
     var productId = UUID.randomUUID();
     var order = new OrderToShip(productId, OrderStatus.CANCELLED, 1);
     repository.save(order);
+    var status = new StatusInput("OUT_FOR_DELIVERY");
 
     var response = RestAssured.given()
                        .accept("application/json")
                        .contentType("application/json")
+                       .body(status)
                        .when()
                        .post("/order/" + order.getId() + "/delivery")
                        .then()
@@ -93,10 +101,12 @@ class SetOrderOutForDeliveryControllerIntegrationTest {
     var productId = UUID.randomUUID();
     var order = new OrderToShip(productId, OrderStatus.OUT_FOR_DELIVERY, 1);
     repository.save(order);
+    var status = new StatusInput("OUT_FOR_DELIVERY");
 
     var response = RestAssured.given()
                        .accept("application/json")
                        .contentType("application/json")
+                       .body(status)
                        .when()
                        .post("/order/" + order.getId() + "/delivery")
                        .then()
@@ -116,10 +126,12 @@ class SetOrderOutForDeliveryControllerIntegrationTest {
     var productId = UUID.randomUUID();
     var order = new OrderToShip(productId, OrderStatus.IN_DELIVERY, 1);
     repository.save(order);
+    var status = new StatusInput("OUT_FOR_DELIVERY");
 
     var response = RestAssured.given()
                        .accept("application/json")
                        .contentType("application/json")
+                       .body(status)
                        .when()
                        .post("/order/" + order.getId() + "/delivery")
                        .then()
@@ -139,10 +151,12 @@ class SetOrderOutForDeliveryControllerIntegrationTest {
     var productId = UUID.randomUUID();
     var order = new OrderToShip(productId, OrderStatus.DELIVERED, 1);
     repository.save(order);
+    var status = new StatusInput("OUT_FOR_DELIVERY");
 
     var response = RestAssured.given()
                        .accept("application/json")
                        .contentType("application/json")
+                       .body(status)
                        .when()
                        .post("/order/" + order.getId() + "/delivery")
                        .then()
@@ -157,14 +171,40 @@ class SetOrderOutForDeliveryControllerIntegrationTest {
   }
 
   @Test
-  void ensureGivenOrderId_whenOrderHasStatusNotSent_thenShouldGetOk() {
+  void ensureGivenOrderId_whenOrderStatusIsNotGiven_thenShouldGetBadRequest() {
     var productId = UUID.randomUUID();
     var order = new OrderToShip(productId, OrderStatus.NOT_SENT, 1);
     repository.save(order);
+    var status = new StatusInput(null);
 
     var response = RestAssured.given()
                        .accept("application/json")
                        .contentType("application/json")
+                       .body(status)
+                       .when()
+                       .post("/order/" + order.getId() + "/delivery")
+                       .then()
+                       .extract()
+                       .response();
+
+    var responseBody = response.getBody().jsonPath();
+    var expectedMessageError =
+        "Order status is not given. Cannot set set a new status!";
+    assertThat(response.getStatusCode(), is(400));
+    assertThat(responseBody.get("message"), is(expectedMessageError));
+  }
+
+  @Test
+  void ensureGivenOrderId_whenOrderHasStatusNotSent_thenShouldGetOk() {
+    var productId = UUID.randomUUID();
+    var order = new OrderToShip(productId, OrderStatus.NOT_SENT, 1);
+    repository.save(order);
+    var status = new StatusInput("OUT_FOR_DELIVERY");
+
+    var response = RestAssured.given()
+                       .accept("application/json")
+                       .contentType("application/json")
+                       .body(status)
                        .when()
                        .post("/order/" + order.getId() + "/delivery")
                        .then()
@@ -180,10 +220,12 @@ class SetOrderOutForDeliveryControllerIntegrationTest {
     var productId = UUID.randomUUID();
     var order = new OrderToShip(productId, OrderStatus.NOT_SENT, 1);
     repository.save(order);
+    var status = new StatusInput("OUT_FOR_DELIVERY");
 
     var response = RestAssured.given()
                        .accept("application/json")
                        .contentType("application/json")
+                       .body(status)
                        .when()
                        .post("/order/" + order.getId() + "/delivery")
                        .then()
